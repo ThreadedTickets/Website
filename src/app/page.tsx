c9c365a965510367ad413290ae0529d763c742d0
ThatBadName <thatbadname@outlook.com>
2025-05-31 08:46:28 +0100

    added links to terms and privacy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,32 +8,42 @@ export default function Home() {
   return (
     <div className="bg-background h-screen text-text">
       <div className="flex flex-col justify-center h-full items-center gap-4">
-        <div className="text-center">
-          <p className="text-6xl font-bold">Threaded</p>
-          <p className="text-2xl">Ticketing without limits</p>
-        </div>
-        <div className="flex flex-col gap-4 w-fit justify-center">
-          <FancyButton
-            text="Invite Threaded"
-            onClick={() =>
-              window.open(
-                "https://discord.com/oauth2/authorize?client_id=1068627569760550994",
-                "_blank"
-              )
-            }
-          />
-          <div className="flex gap-4">
-            <Button
-              text="Support"
+        <div className="flex flex-col justify-center gap-4 grow">
+          <div className="text-center">
+            <p className="text-6xl font-bold">Threaded</p>
+            <p className="text-2xl">Ticketing without limits</p>
+          </div>
+          <div className="flex flex-col gap-4 w-fit justify-center">
+            <FancyButton
+              text="Invite Threaded"
               onClick={() =>
-                window.open("https://discord.gg/9jFqS5H43Q", "_blank")
+                window.open(
+                  "https://discord.com/oauth2/authorize?client_id=1068627569760550994",
+                  "_blank"
+                )
               }
             />
-            <Button
-              text="Placeholders"
-              onClick={() => router.push("/placeholders")}
-            />
+            <div className="flex gap-4">
+              <Button
+                text="Support"
+                onClick={() =>
+                  window.open("https://discord.gg/9jFqS5H43Q", "_blank")
+                }
+              />
+              <Button
+                text="Placeholders"
+                onClick={() => router.push("/placeholders")}
+              />
+            </div>
           </div>
+        </div>
+        <div className="flex gap-4 text-text/50">
+          <a href="/terms" className="hover:text-text/70">
+            <p>Terms</p>
+          </a>
+          <a href="/privacy" className="hover:text-text/70">
+            <p>Privacy</p>
+          </a>
         </div>
       </div>
     </div>
